d45e59cd19b21e57fb17bc37efeb75c88baa5f18
Luke Welch <luke.andrew.welch@gmail.com>
2018-09-24 18:55:46 -0400

    Bug fixes and documentation updates

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# Diamond_Challenge
+# Diamond Challenge
+
+The diamond_challenge module takes a letter as an input, either on the command line, or via the
+the keybord after the start of the script, and prints a diamond, starting with 'A' at the top,
+and the supplied letter at the widest point.
+
+The following is an example usage for supplying a letter
+on the command line:
+```
+python diamond_challenge.py E
+```
+The script will then print out the following:
+```
+    A
+   B B
+  C   C
+ D     D
+E       E
+ D     D
+  C   C
+   B B
+    A
+```
+
+Input can also be accepted at run-time. By using `python diamond_challenge.py`, the program will request an input via the following prompt:
+
+```
+    Enter a letter to create the diamond:
+```
+If a valid input is given, the program will print the diamond. If not, an error message will be generated, and the program will exit. This could easily be customized to handle errors the way the user wants.
+
+---
+
+## Functions:
+### **print_diamond(letter):**
+>**Inputs:** *str* letter: A capital letter, which will be used as
+the widest point of the diamond.
+
+print_diamond takes the an input of a capital letter, and prints a diamond of letters starting with 'A' at the top
+and bottom point, and the provided letter at the right and left points. This is the most generic
+function that calls all verificationand building functions. It is also responsible for printing
+the resulting diamond.
+
+For example, if 'D' is provided, the following will be printed:
+```
+   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A
+```
+
+If 'A' is input, a diamond cannot be generated, so a single line with 'A' is printed.
+
+---
+
+### **build_diamond(letter):**
+>**Inputs:** *str* validated_letter: A capital letter, that will be used to generate the
+list of strings needed to print out the diamond.
+
+>**Returns:** A list a strings that contains the correct spacing for printing
+the diamond.
+
+build_diamond is used to generate the list of strings needed to print the diamond structure.
+It takes a single argument of a letter (in string format), and returns a list of strings.
+
+This list of strings can then be printed with newline characters (using join) to output the
+diamond structure.
+
+---
+
+### **validate_input(input_string):**
+>**Inputs:** *str* input_string: A string input to the program for validation
+that it will work wiht the diamond_challenge program.
+
+>**Returns:** *True* if the string can be used to create a diamond.
+*False* if cannot be used.
+
+---
+## Unit Tests
+Unit tests are provided with the program to test the functionality.
+They can be run from the command line by entering:
+```
+python -m unittest diamond_challenge_unittest
+```
--- diamond_challenge.py
+++ diamond_challenge.py
@@ -1,18 +1,48 @@
+"""
+The diamond_challenge module takes a letter as an input, either on the command line, or via the
+the keybord after the start of the script, and prints a diamond, starting with 'A' at the top,
+and the supplied letter at the widest point.
+
+The following is an example usage:
+```
+python diamond_challenge.py E
+```
+The script will then print out the following:
+```
+    A
+   B B
+  C   C
+ D     D
+E       E
+ D     D
+  C   C
+   B B
+    A
+```
+"""
+
 import sys
 
 def print_diamond(letter):
     """
+    :param str letter: A capital letter, which will be used as
+    the widest point of the diamond.
+
     print_diamond takes the an input of a capital letter, and prints a diamond of letters starting with 'A' at the top
-    and bottom point, and the provided letter at the right and left points. 
-    
+    and bottom point, and the provided letter at the right and left points. This is the most generic
+    function that calls all verificationand building functions. It is also responsible for printing
+    the resulting diamond.
+
     For example, if 'D' is provided, the following will be printed:
-        A
-       B B
-      C   C
-     D     D
-      C   C
-       B B
-        A
+    ```
+       A
+      B B
+     C   C
+    D     D
+     C   C
+      B B
+       A
+    ```
 
     If 'A' is input, a diamond cannot be generated, so a single line with 'A' is printed.
     """
@@ -21,34 +51,43 @@ def print_diamond(letter):
         diamond = build_diamond(letter)
         print('\n'.join(diamond))
 
-def build_diamond(letter):
+def build_diamond(validated_letter):
     """
+    >:param str validated_letter: A capital letter, that will be used to generate the
+    list of strings needed to print out the diamond.
+
+    >**Returns:** A list a strings that contains the correct spacing for printing
+    the diamond.
+
     build_diamond is used to generate the list of strings needed to print the diamond structure.
     It takes a single argument of a letter (in string format), and returns a list of strings.
 
     This list of strings can then be printed with newline characters (using join) to output the
     diamond structure.
+
     """
     a_ascii = ord('A')
-    rows = ord(letter) - a_ascii + 1
+    rows = ord(validated_letter) - a_ascii + 1
     diamond = []
 
     for x in list(range(rows)) + list(reversed(range(rows-1))):
         if x == 0:
-            diamond.append('{: <{w1}}{letter}'.format('', w1=rows-1, letter=chr(a_ascii+x)))
+            diamond.append('{: <{w1}}{current_letter}'.format('', w1=rows-1, current_letter=chr(a_ascii+x)))
         else:
-            diamond.append('{: <{w1}}{letter}{: <{w2}}{letter}'.format('', '', w1=rows-x-1, letter=chr(a_ascii+x), w2=x*2-1))
+            diamond.append('{: <{w1}}{current_letter}{: <{w2}}{current_letter}'.format('', '', w1=rows-x-1, current_letter=chr(a_ascii+x), w2=x*2-1))
     return diamond
 
-def validate_input(letter):
+def validate_input(input_string):
     """
-    validate_input has an input of a string. It check that the string length is 1 (a single letter)
-    and verifies that the letter is capital.
+    :param str input_string: A string input to the program for validation
+    that it will work wiht the diamond_challenge program.
 
-    If the input is valid, validate_input returns True. Otherwise, it returns False.
+    :return: *True* if the string can be used to create a diamond.
+    *False* if cannot be used.
+
     """
-    if not isinstance(letter, str) or len(letter) != 1 \
-    or ord(letter) < ord('A') or ord(letter) > ord('Z'):
+    if not isinstance(input_string, str) or len(input_string) != 1 \
+    or ord(input_string) < ord('A') or ord(input_string) > ord('Z'):
         print("\nInvalid input! Please input a single capital letter (A-Z).\n")
         return False
     return True
